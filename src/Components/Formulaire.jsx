3a443e0058e849e28db0a6f4d6ecc2d5d9ac3a17
Salim Mouttaki <MOU@inokscapital.com>
2022-05-12 14:09:40 +0200

    first commit

---
+++ src/Components/Formulaire.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import './Formulaire.css'
+import Fractale from './Fractale'
+import axios from 'axios';
+
+
+
+function Formulaire() {
+
+
+    let [points, setPoints] = useState([{x: 0 , y: 1},  {x: 100 , y: 200}])
+    let [segmentsize, setSegmentSize] = useState(0);
+    let [degre, setDegre] = useState(0);
+  
+    async function SendInformationToApi(){
+        const requestOptions = {
+            method: 'POST',
+            headers:{'Content-Type': 'application/json'},
+            body: { degre: degre, segmentSize: 12}
+        }
+        console.log("TEST input", requestOptions)
+        console.log("TEST input2", segmentsize)
+        let response = await axios.post('https://localhost:7003/api/Fractale/', { degre: degre, segmentSize: segmentsize})
+
+        setPoints(response.data);
+    }
+    
+  return (
+    <div className="form">
+      <div className="title">Bienvenu</div>
+      <div className="subtitle">Entrez les paramètres</div>
+      <div className="input-container ic1">
+        <input id="segmentsize" className="input" type="text" placeholder=" " onChange={(change) => {setSegmentSize(change.currentTarget.value)}} />
+        <div className="cut"></div>
+        <label htmlfor="texte" className="placeholder">Taille du Segment (px)</label>
+      </div>
+      <div className="input-container ic2">
+        <input id="degre" className="input" type="text" placeholder=" " onChange={(change) => setDegre(change.currentTarget.value)} />
+        <div className="cut"></div>
+        <label className="placeholder">Degré du fractale</label>
+      </div>
+      <button onClick={SendInformationToApi} type="text" className="submit">Valider</button>
+      <button onClick={SendInformationToApi} type="text" className="submit">Zoom</button>
+
+      <div className="input-container ic2">
+          {}
+    </div>
+        <Fractale points={points}/>
+    </div>
+    
+  )
+}
+
+
+
+export default Formulaire
---
+++ src/Components/Fractale.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './Fractale.css'
+import { Component } from 'react';
+
+
+
+class Fractale extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+          points: [],
+          firstPoint: null,
+          nextPoint: null
+        } 
+      };
+      render(){
+          console.log("Ici c'est salim",this.props.points)
+    return (<>
+    <div className='Fractale'>
+    <h1>Fractale</h1>
+    <svg>
+        {this.props.points.map((point, i) => {
+            if(i === this.props.points.length -1)
+                return <>
+                <line x1={this.props.points[i].x}  y1={this.props.points[i].y} x2={this.props.points[0].x} y2={this.props.points[0].y} ></line>
+                </>
+                  
+            return <>
+            <line x1={this.props.points[i].x} y1={this.props.points[i].y} x2={this.props.points[i + 1].x} y2={this.props.points[i+1].y} ></line>
+            </>
+            
+        })}
+    </svg>
+    </div>
+    
+    </>
+    )
+}
+}
+
+export default Fractale
